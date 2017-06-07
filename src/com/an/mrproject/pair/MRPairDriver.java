ab47b02ed1615241566fdd9742943c2f3d189b25
Anish Panthi <anish.panthi@gmail.com>
2017-06-07 11:30:54 -0500

    Driver Class Added.

--- src/com/an/mrproject/pair/MRPairDriver.java
+++ src/com/an/mrproject/pair/MRPairDriver.java
@@ -1,23 +1,34 @@
 package com.an.mrproject.pair;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class MRPairDriver extends Configured implements Tool {
+public class MRPairDriver {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		Configuration configuration = new Configuration();
-		int exitCode = ToolRunner.run(configuration, new MRPairDriver(), args);
-		System.exit(exitCode);
 
+		Job job = new Job(configuration, "MRProject Pair Approach Finding Relative Frequency");
+
+		job.setJarByClass(MRPairDriver.class);
+		job.setNumReduceTasks(2);
+		job.setMapperClass(PairMapper.class);
+		job.setPartitionerClass(DataPartitioner.class);
+		job.setReducerClass(PairReducer.class);
+
+		job.setMapOutputKeyClass(PairWritable.class);
+		job.setMapOutputValueClass(IntWritable.class);
+
+		job.setOutputKeyClass(PairWritable.class);
+		job.setOutputValueClass(FloatWritable.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
-
-	@Override
-	public int run(String[] arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
