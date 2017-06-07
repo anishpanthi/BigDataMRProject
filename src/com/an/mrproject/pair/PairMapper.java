55591a64edc6e33b1c9a901c73a3c1f24030667d
Anish Panthi <anish.panthi@gmail.com>
2017-06-07 11:23:11 -0500

    Mapper/Reducer Added.

--- src/com/an/mrproject/pair/PairMapper.java
+++ src/com/an/mrproject/pair/PairMapper.java
@@ -3,6 +3,7 @@ package com.an.mrproject.pair;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -16,13 +17,56 @@ public class PairMapper extends Mapper<Object, Text, PairWritable, IntWritable> 
 			InterruptedException {
 		combiner = new HashMap<>();
 	}
-	
-	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-		
+
+	public void map(Object objKey, Text value, Context context)
+			throws IOException, InterruptedException {
+		String line = value.toString();
+		String data = line.substring(line.indexOf(" ") + 1, line.length());
+
+		String[] values = data.split("\\s+");
+
+		for (int i = 0; i < values.length - 1; i++) {
+			String[] neighbours = getNeighbours(i, values[i], values);
+
+			for (String neighbour : neighbours) {
+				if (neighbour == null) {
+					break;
+				}
+				String key = values[i];
+
+				if (combiner.containsKey(key)) {
+					Map<String, Integer> items = combiner.get(key);
+					if (items.containsKey(neighbour)) {
+						items.put(neighbour, items.get(neighbour) + 1);
+					} else {
+						items.put(neighbour, 1);
+					}
+				} else {
+					Map<String, Integer> items = new HashMap<>();
+					items.put(neighbour, 1);
+					combiner.put(key, items);
+				}
+			}
+		}
 	}
-	
-	protected void cleanup(Context context) throws IOException, InterruptedException{
-		
+
+	protected void cleanup(Context context) throws IOException,
+			InterruptedException {
+
+		Set<String> keys = combiner.keySet();
+
+		for (String key : keys) {
+			Map<String, Integer> pairs = combiner.get(key);
+			Set<String> values = pairs.keySet();
+			int count = 0;
+
+			for (String value : values) {
+				count += pairs.get(value);
+				context.write(new PairWritable(key, value), new IntWritable(
+						pairs.get(value)));
+			}
+			context.write(new PairWritable(key, "*"), new IntWritable(count));
+		}
 	}
 
 	private String[] getNeighbours(int index, String item, String[] data) {
--- src/com/an/mrproject/pair/PairReducer.java
+++ src/com/an/mrproject/pair/PairReducer.java
@@ -1,5 +1,30 @@
 package com.an.mrproject.pair;
 
-public class PairReducer {
+import java.io.IOException;
 
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class PairReducer extends
+		Reducer<PairWritable, IntWritable, PairWritable, FloatWritable> {
+
+	private int marginalCounter = 0;
+
+	public void reduce(PairWritable key, Iterable<IntWritable> values,
+			Context context) throws IOException, InterruptedException {
+
+		if (key.getY().equals("*")) {
+			int temp = 0;
+			for (IntWritable value : values) {
+				temp += value.get();
+			}
+			marginalCounter = temp;
+		} else {
+			for (IntWritable value : values) {
+				float frequency = (float) value.get() / marginalCounter;
+				context.write(key, new FloatWritable(frequency * 100));
+			}
+		}
+	}
 }
