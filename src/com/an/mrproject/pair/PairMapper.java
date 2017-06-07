069fac1f3f08ae802355fead8d6c5b27dfef09c9
Anish Panthi <anish.panthi@gmail.com>
2017-06-07 10:28:29 -0500

    Mapping Added.

--- src/com/an/mrproject/pair/PairMapper.java
+++ src/com/an/mrproject/pair/PairMapper.java
@@ -1,6 +1,8 @@
 package com.an.mrproject.pair;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -8,6 +10,35 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class PairMapper extends Mapper<Object, Text, PairWritable, IntWritable> {
 
-	HashMap<String, HashMap<String, Integer>> combiner = null;
+	Map<String, Map<String, Integer>> combiner = null;
+
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		combiner = new HashMap<>();
+	}
+	
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+		
+	}
+	
+	protected void cleanup(Context context) throws IOException, InterruptedException{
+		
+	}
+
+	private String[] getNeighbours(int index, String item, String[] data) {
+
+		int length = data.length - (index + 1);
+		String[] neighbours = new String[length];
+
+		int j = 0;
+		for (int i = index + 1; i < data.length; i++) {
+			if (data[i].equals(item)) {
+				break;
+			}
+			neighbours[j++] = data[i];
+		}
+
+		return neighbours;
+	}
 
 }
